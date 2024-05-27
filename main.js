ece87bcfc03d6103ea240a6fed64bd34578cd728
111USZ <161062914+111USZ@users.noreply.github.com>
2024-05-27 15:19:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let myConfirm = confirm("Savatga maxsulot qoshilsinmi")
+let arr = ['Non', 'Tuhum', 'Shakar']
+let product = 'coffee'
+if (myConfirm === true) {
+    arr.push(product)
+    console.log(arr)
+}
+
+else {
+    console.log(arr)
+}
